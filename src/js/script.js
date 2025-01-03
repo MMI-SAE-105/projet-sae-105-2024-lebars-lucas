767ad2000b57beb82d2915da3ef4f4d93f2dd1d3
Lucas <lucas.lebars@edu.univ-fcomte.fr>
2025-01-03 15:58:40 +0100

    ajout du js pour le menu

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+const menuButton = document.querySelector('.hamburger-icon-menu');
+const menu = document.getElementById('mainNav');
+
+menuButton.addEventListener('click', () => {
+  const isHidden = menu.getAttribute('aria-hidden') === 'true';
+  menu.setAttribute('aria-hidden', !isHidden);
+});
+
+// Délégation d'événements pour l'image "logoaccueil.png"
+document.addEventListener('click', function(event) {
+  if (event.target.src.endsWith('/assets/icons/hamburger-icon.svg')) {
+    menu.setAttribute('aria-hidden', 'true');
+  }
+});
